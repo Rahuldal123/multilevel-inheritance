5106ea7016653c3c1123a35cfd144e03c51f53a3
Rahuldal123 <rahuldalwale55@gmail.com>
2022-09-17 11:38:52 +0530

    Add project files.

---
+++ ConsoleApp11/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    abstract public class Shape
+    {
+        public abstract void Area();
+    }
+    public class Circle : Shape
+
+    {
+        private double radius, area;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override void Area()
+        {
+            area = 3.14 * radius * radius;
+        }
+        public override string ToString()
+        {
+            return $"area of circle is {area}";
+
+        }
+
+    }
+    public class  Reactangle :Shape
+    {
+        private double length, breadth, area;
+
+        public Reactangle(double length, double breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+            
+        }
+        public override void Area()
+        {
+            area = length * breadth;
+        }
+        public override string ToString()
+        {
+            return $"Area of rectangle is {area}";
+        }
+    }
+    public class Sphere :Shape
+    {
+        private double radius, area;
+
+        public Sphere(double radius)
+        {
+            this.radius= radius;
+        }
+        public override void Area()
+        {
+            area = 4 * 3.14 * radius * radius;
+
+        }
+        public override string ToString()
+        {
+            return $"Area of sphere is {area}";
+        }
+
+
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(20);
+            c1.Area();
+            Console.WriteLine(c1);
+
+
+            Reactangle R1 = new Reactangle(12.5,15.6);
+            R1.Area();
+            Console.WriteLine(R1);
+
+            Sphere S1 = new Sphere(23.5);
+            S1.Area();
+            Console.WriteLine(S1);
+
+
+
+        }
+    }
+}
